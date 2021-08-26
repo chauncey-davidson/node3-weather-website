fadecc67565397bd9a3f0089cd33d5b70b0eb436
chauncey-davidson <chahavidson@gmail.com>
2021-08-26 13:57:32 -0700

    Add humidity text

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -1,18 +1,5 @@
 const request = require('postman-request');
 
-// request({url: url, json: true}, (error, response) => {
-
-//   if (error) {
-//     console.log('Unable to connect to weather service.');
-//   } else if (response.body.error) {
-//     console.log('Unable to find location.');
-//   } else {
-//     const data = response.body.current;
-//     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + data.temperature + ' degrees out. It feels like ' + data.feelslike + ' degrees out.');
-//   }
-  
-// });
-
 const forecast = (latitude, longitude, callback) => {
 
   const url = 'http://api.weatherstack.com/current?access_key=adc6a0ac0c8b2af22a4d463bd391a397&query=' + longitude + ',' + latitude + '&units=f';
@@ -23,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location.');
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. ' + body.current.temperature + ' degrees, feels like ' + body.current.feelslike + ' degrees.');
+      callback(undefined, body.current.weather_descriptions[0] + '. ' + body.current.temperature + ' degrees, feels like ' + body.current.feelslike + ' degrees. The humidity is ' + body.current.humidity + '.');
     }
   });
 
